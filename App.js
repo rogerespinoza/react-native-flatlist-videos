a537f67f512f9cd66978e1d0b783bd9903ccad67
Roger Espinoza <rj.espinoza44@gmail.com>
2023-01-20 22:58:35 -0500

    flatlist videos

--- App.js
+++ App.js
@@ -1,117 +1,215 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  FlatList,
+  Dimensions,
+  TouchableOpacity,
+  Image,
 } from 'react-native';
+// import { Video } from 'expo-av';
+import Video from 'react-native-video';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const { height, width } = Dimensions.get('window');
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+const cellHeight = height * 0.6;
+const cellWidth = width;
+
+const viewabilityConfig = {
+  itemVisiblePercentThreshold: 80,
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+const initialItems = [
+  {
+    id: 0,
+    url: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4',
+    poster:
+      'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerBlazes.jpg',
+  },
+  {
+    id: 1,
+    url: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+    poster:
+      'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg',
+  },
+  {
+    id: 2,
+    url: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4',
+    poster:
+      'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerMeltdowns.jpg',
+  },
+];
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+class Item extends React.PureComponent {
+  componentWillUnmount() {
+    if (this.video) {
+      // this.video.unloadAsync();
+    }
+  }
+
+  async play() {
+    const status = await this.video.getStatusAsync();
+    if (status.isPlaying) {
+      return;
+    }
+    return this.video.setNativeProps({ paused: true });
+  }
+
+  pause() {
+    if (this.video) {
+      this.video.setNativeProps({ paused: false });
+    }
+  }
+
+  render() {
+    const { id, poster, url } = this.props;
+    const uri = url + '?bust=' + id;
+    return (
+      <View style={styles.cell}>
+        <Image
+          source={{
+            uri: poster,
+            cache: 'force-cache',
+          }}
+          style={[styles.full, styles.poster]}
+        />
+        <Video
+          ref={ref => {
+            this.video = ref;
+          }}
+          source={{ uri }}
+          // isMuted={false}
+          shouldPlay={true}
+          resizeMode="cover"
+          style={styles.full}
+        />
+        <View style={styles.overlay}>
+          <Text style={styles.overlayText}>Item no. {id}</Text>
+          <Text style={styles.overlayText}>Overlay text here</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+export default class App extends React.PureComponent {
+  state = {
+    items: [],
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+  constructor(props) {
+    super(props);
+    this.cellRefs = {};
+  }
+
+  componentDidMount() {
+    this.loadItems();
+    setTimeout(this.loadItems, 1000);
+    setTimeout(this.loadItems, 1100);
+    setTimeout(this.loadItems, 1200);
+    setTimeout(this.loadItems, 1300);
+  }
+
+  _onViewableItemsChanged = props => {
+    const changed = props.changed;
+    changed.forEach(item => {
+      const cell = this.cellRefs[item.key];
+      if (cell) {
+        if (item.isViewable) {
+          cell.play();
+        } else {
+          cell.pause();
+        }
+      }
+    });
+  };
+
+  loadItems = () => {
+    const start = this.state.items.length;
+    const newItems = initialItems.map((item, i) => ({
+      ...item,
+      id: start + i,
+    }));
+    const items = [...this.state.items, ...newItems];
+    this.setState({ items });
+  };
+
+  _renderItem = ({ item }) => {
+    return (
+      <Item
+        ref={ref => {
+          this.cellRefs[item.id] = ref;
+        }}
+        {...item}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+    );
+  };
+
+  render() {
+    const { items } = this.state;
+    return (
+      <View style={styles.container}>
+        <FlatList
+          style={{ flex: 1 }}
+          data={items}
+          renderItem={this._renderItem}
+          keyExtractor={item => item.id}
+          onViewableItemsChanged={this._onViewableItemsChanged}
+          initialNumToRender={3}
+          maxToRenderPerBatch={3}
+          windowSize={5}
+          getItemLayout={(_data, index) => ({
+            length: cellHeight,
+            offset: cellHeight * index,
+            index,
+          })}
+          viewabilityConfig={viewabilityConfig}
+          removeClippedSubviews={true}
+          ListFooterComponent={
+            <TouchableOpacity onPress={this.loadItems}>
+              <Text style={{ padding: 30 }}>Load more</Text>
+            </TouchableOpacity>
+          }
+        />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  cell: {
+    width: cellWidth - 20,
+    height: cellHeight - 20,
+    backgroundColor: '#eee',
+    borderRadius: 20,
+    overflow: 'hidden',
+    margin: 10,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    backgroundColor: 'rgba(0,0,0,0.4)',
+    padding: 40,
   },
-  highlight: {
-    fontWeight: '700',
+  full: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+  },
+  poster: {
+    resizeMode: 'cover',
+  },
+  overlayText: {
+    color: '#fff',
   },
 });
-
-export default App;
---
+++ README.md
@@ -0,0 +1,6 @@
+## source:
+https://medium.com/@jenshandersson/react-native-optimized-flatlist-of-videos-bb048cb696db
+autor: Jens Andersson 
+
+
+Implementation with react-native-cli
